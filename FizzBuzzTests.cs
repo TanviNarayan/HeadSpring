c3f73c300324f1a500f9accc1a8728b5709a1141
TanviNarayan <32529189+TanviNarayan@users.noreply.github.com>
2021-02-08 09:46:22 -0600

    Add files via upload

---
+++ FizzBuzzTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HeadSpring.Core.Tests
+{
+    [TestClass()]
+    public class FizzBuzzTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FizzBuzzStrategyShouldNotBeEmpty()
+        {
+            var game = new FizzBuzz();
+            game.Process(null);
+        }
+
+        [TestMethod()]
+        public void FizzBuzzTest()
+        {            
+            var game = new FizzBuzz();
+            game.Process(new RangeNumbers(1,100));
+        }
+
+    }
+}
