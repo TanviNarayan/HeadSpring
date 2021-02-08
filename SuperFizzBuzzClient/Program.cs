d9ce9faf00b0113ae0b89fa8cebfbb9a1426790e
TanviNarayan <32529189+TanviNarayan@users.noreply.github.com>
2021-02-08 10:20:33 -0600

    Add files via upload

---
+++ SuperFizzBuzzClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using HeadSpring.Core;
+
+namespace SuperFizzBuzzClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var superFizzBuzz = new HeadSpring.Core.FizzBuzz(new Dictionary<int, string> { { 3, "Fizz" }, { 7, "Buzz" }, { 38, "Bazz" } });
+            var op = superFizzBuzz.Process(new RangeNumbers(12, 145));
+            foreach (var n in op)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
