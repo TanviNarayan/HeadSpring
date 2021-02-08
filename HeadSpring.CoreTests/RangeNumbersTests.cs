187b2be5d6588f44e7e626df61541edea05e3272
TanviNarayan <32529189+TanviNarayan@users.noreply.github.com>
2021-02-08 10:05:10 -0600

    Add files via upload

---
+++ HeadSpring.CoreTests/RangeNumbersTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HeadSpring.Core.Tests
+{
+    [TestClass()]
+    public class RangeNumbersTests
+    {
+
+        [TestMethod()]
+        public void ReturnRangeOfNumbersInUpdateRange()
+        {
+            var range = new RangeNumbers(1, 100);
+            range.UpdateRange(2, 20);
+            foreach (var number in range.GetNumbers())
+            {
+                if (number < 2 || number > 20)
+                {
+                    Assert.Fail("Invalid number returned");
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void ReturnRangeOfNumbersInCtor()
+        {
+            var range = new RangeNumbers(1, 100);
+            foreach (var number in range.GetNumbers())
+            {
+                if (number < 1 || number > 100)
+                {
+                    Assert.Fail("Invalid number returned");
+                }
+            }
+        }
+    }
+}
