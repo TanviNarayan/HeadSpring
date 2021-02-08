1c08dc37da12467db9390e7e4f7cd83e4a5d7208
TanviNarayan <32529189+TanviNarayan@users.noreply.github.com>
2021-02-08 10:21:46 -0600

    Add files via upload

---
+++ BasicFizzBuzzClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HeadSpring.Core;
+
+namespace BasicFizzBuzzClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var basicFizzBuzz = new HeadSpring.Core.FizzBuzz();
+            var op = basicFizzBuzz.Process(new RangeNumbers(1, 100));
+            foreach (var n in op)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
